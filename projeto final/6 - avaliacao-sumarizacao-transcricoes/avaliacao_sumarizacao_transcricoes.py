12085a558ec8a6f4bffa6b8b19f9b4c1f86d83b4
Leandro Carísio <carisio@gmail.com>
2024-06-29 15:27:24 -0300

    Atualiza relatório com dados sobre alucinação

--- projeto final/6 - avaliacao-sumarizacao-transcricoes/avaliacao_sumarizacao_transcricoes.py
+++ projeto final/6 - avaliacao-sumarizacao-transcricoes/avaliacao_sumarizacao_transcricoes.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-
+# Métricas precisão e recall:
 evaluations = []
 with open('evaluation_results.jsonl', encoding='utf-8') as fin:
     for line in fin:
@@ -18,4 +18,22 @@ df_metricas = pd.DataFrame({
     "precisao": [r['metricas']['precisao'] for r in evaluations],
 })
 
-df_metricas.describe(percentiles=[0.05, 0.25, 0.5, 0.75, 0.95])
+# Resultados das alucinação:
+hallucinations = []
+with open('hallucination_results.jsonl', encoding='utf-8') as fin:
+    for line in fin:
+        hallucinations.append(json.loads(line))
+
+
+df_alucinacao = pd.DataFrame({
+    "id": [h['id'] for h in hallucinations],
+    "alucinacao": [h['metricas']['proporcao_alucinacao'] for h in hallucinations]
+})
+
+# Agrega num único df a precisão, recall e alucinação:
+df_metricas = pd.merge(df_metricas, df_alucinacao, on='id', how='outer')
+
+print(df_metricas.describe(percentiles=[0.05, 0.25, 0.5, 0.75, 0.95]))
+
+ids_sem_dados_alucinacao = df_metricas[df_metricas['alucinacao'].isna()].id.to_list()
+print('Ids sem dados de alucinação:', ids_sem_dados_alucinacao)
