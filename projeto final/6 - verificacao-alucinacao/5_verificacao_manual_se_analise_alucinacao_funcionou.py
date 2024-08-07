962b42fae6949fc71973797fa2fd90a9794681ec
Leandro Carísio <carisiof@tcu.gov.br>
2024-08-07 12:12:24 -0300

    Adicionada análises até id=6

--- projeto final/6 - verificacao-alucinacao/5_verificacao_manual_se_analise_alucinacao_funcionou.py
+++ projeto final/6 - verificacao-alucinacao/5_verificacao_manual_se_analise_alucinacao_funcionou.py
@@ -36,9 +36,9 @@ def salvar_resultado_experimento_com_analise_alucinacao():
     salvar_jsonl(arquivo_experimento_com_analise_alucinacao.format(parte=4), resultado_experimento[150:])
     print('* Arquivo jsonl com o resultado atualizado *')
     
-def avalia_manualmente_opiniao(opniao):
-    print('\n'*10)
-    print('................')
+def avalia_manualmente_opiniao(opniao, pre_texto_informativo):
+    print('\n\n\n................')
+    print(f"\t\t{pre_texto_informativo}")
     texto_opiniao = opiniao['opiniao']
     chunks_proximos = opiniao['chunks_proximos']
     analise_alucinacao = ">>>>>>>> SIM <<<<<<<<" if opiniao['eh_alucinacao'] else "não"
@@ -63,17 +63,22 @@ def avalia_manualmente_opiniao(opniao):
 # Carrega resultado do experimento
 resultado_experimento = carregar_resultado_experimento_com_analise_alucinacao()
 for resultado in resultado_experimento:
-    id = resultado['id']
-    print(f'########## TRATANDO ID {id} ##########')
+    id_resultado = resultado['id']
+    print(f'########## TRATANDO ID {id_resultado} ##########')
     
+    num_envolvido = 0
     for envolvido in resultado['metadados_extraidos']['envolvidos']:
+        num_envolvido += 1
         nome = envolvido['nome']
         print(f'\t{nome}')
         
         houve_analise_opinioes = False
+        num_opiniao_envolvido = 0
         for opiniao in envolvido['opinioes']:
+            num_opiniao_envolvido += 1
             if 'concordancia_com_analise_alucinacao' not in opiniao.keys():
-                avalia_manualmente_opiniao(opiniao)
+                pre_texto_informativo = f"Opinião {num_opiniao_envolvido}/{len(envolvido['opinioes'])} de {nome} {num_envolvido}/{len(resultado['metadados_extraidos']['envolvidos'])}, id #{id_resultado}"
+                avalia_manualmente_opiniao(opiniao, pre_texto_informativo)
                 houve_analise_opinioes = True
 
         if houve_analise_opinioes:
